5a6b257a678e3861f09d7d2f2b16dff9cf536a56
SpondonArafin <82678481+SpondonArafin@users.noreply.github.com>
2021-10-14 20:12:35 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,69 @@
 
-song = "";
-song2 = "";
-function draw() {
-    image(video,0,0,300,300);
-}
-
-function setup() {
-    video = createCapture(VIDEO);
-    video.hide()
-
-    canvas = createCanvas(300,500);
-    canvas.center();
-}
-
-
-
-function preload() {
-    song = loadSound("ghost_dj.mp3");
-    song2 = loadSound("neonrave.mp3");
-}
+  
+  
+  
+  
+  
+  
+  song = "";
+   
+  function preload()
+  {
+      song = loadSound("ghost_dj.mp3");
+	  song2 = loadSound("neonrave.mp3")
+  }
+   
+   
+   
+  scoreRightWrist = 0;
+  scoreLeftWrist = 0;
+   
+  rightWristX = 0;
+  rightWristY = 0;
+   
+  leftWristX = 0;
+  leftWristY = 0;
+   
+  function setup() {
+      canvas =  createCanvas(600, 500);
+      canvas.center();
+   
+      video = createCapture(VIDEO);
+      video.hide();
+   
+      poseNet = ml5.poseNet(video, modelLoaded);
+      poseNet.on('pose', gotPoses);
+  }
+   
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+   
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+      }
+   
+      if(scoreLeftWrist > 0.2)
+      {
+          circle(leftWristX,leftWristY,20);
+song.play();
+      }
+   
+	  if(scoreRightWrist > 0.2) {
+		ircle(rightWristX,rightWristY,20);
+		song2.play();
+	  }
+  }
+   
+  function play()
+  {
+      song.play();
+      song.setVolume(1);
+      song.rate(1);
+  }
+   
+  
+  
